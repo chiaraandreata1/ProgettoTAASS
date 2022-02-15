71b6cf70a5abd944053b97e2b6b2d1f9ddc7e7b6
giuseppepimpinella94 <pimpinella.giuseppe@libero.it>
2022-02-15 23:43:27 +0100

    new updates

--- eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/example/eurekaclient/EurekaClientApplication.java
@@ -2,14 +2,13 @@ package com.example.eurekaclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableZuulProxy
 public class EurekaClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
-
 }
