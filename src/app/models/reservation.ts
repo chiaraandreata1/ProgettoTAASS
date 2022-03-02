e8e1a26cbdc8be34a9d8caba92a4a6e0afabb9f1
Chiara <chiara.andreata@edu.unito.it>
2022-03-02 11:36:53 +0100

    2-03-2022

--- src/app/models/reservation.ts
+++ src/app/models/reservation.ts
@@ -1,6 +1,9 @@
+import {Court} from "./court";
+
 export class Reservation {
   id!: number;
   sportReservation!: string;
   dateReservation!: string;
   hourReservation!: number;
+  courtReservation!: Court;
 }
