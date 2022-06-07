7ef3e0fddaea10698540cab353f5556c732b4718
Francesco Ciraolo <francesco.ciraolo@edu.unito.it>
2022-06-07 19:34:56 +0200

    rabbit-mq

--- gateway/postgres-session-delete-all.bat
+++
@@ -1,5 +0,0 @@
-kubectl delete deployment/dbsession
-kubectl delete configmap/postgres-session-config
-kubectl delete pvc/postgres-session-pv-claim
-kubectl delete pv/postgres-session-pv-volume
-kubectl delete service/dbsession
--- sessiodb/postgres-session-apply.bat
+++
@@ -1,4 +0,0 @@
-kubectl apply -f postgres-session-configmap.yaml
-kubectl apply -f postgres-session-deployment.yaml
-kubectl apply -f postgres-session-service.yaml
-kubectl apply -f postgres-session-storage.yaml
